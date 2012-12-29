100cadf62db0c0e375b1eb22bc08fd837858bb4b
hhc <hhcsxx@gmail.com>
2012-12-29 11:51:47 +0800

    用内嵌代码的形式将Spring增加进去了

--- src/main/java/net/chuangdie/lhb/bean/ComImpl.java
+++ src/main/java/net/chuangdie/lhb/bean/ComImpl.java
@@ -1,11 +1,41 @@
 package net.chuangdie.lhb.bean;
 
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
+
 public class ComImpl implements Com
 {
+	private TransactionTemplate transactionTemplate;
+
 	public void insertAll(Shopping shopping, Transaction trans) throws Exception
 	{
 		shopping.insert(shopping);
-		throw new Exception();
-		// trans.insert(trans);
+		trans.insert(trans);
 	}
+
+	public void enrollStudentInCourse(final Shopping shopping, final Transaction trans)
+	{
+		transactionTemplate.execute(new TransactionCallback()
+		{
+			public Object doInTransaction(TransactionStatus ts)
+			{
+				try
+				{
+					shopping.insert(shopping);
+					trans.insert(trans);
+				} catch (Exception e)
+				{
+					ts.setRollbackOnly();
+				}
+				return null;
+			}
+		});
+	}
+
+	public void setTransactionTemplate(TransactionTemplate transactionTemplate)
+	{
+		this.transactionTemplate = transactionTemplate;
+	}
+
 }
